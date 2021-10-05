8573c6613c04f240c1133e88590ea144ba68579a
EltonCN <43186596+EltonCN@users.noreply.github.com>
2021-10-05 19:08:36 -0300

    Tentativa de criar a trajetória

--- Space Pioneers/Assets/Scripts/Gravity.cs
+++ Space Pioneers/Assets/Scripts/Gravity.cs
@@ -30,20 +30,37 @@ public class Gravity : MonoBehaviour
  
     void FixedUpdate()
     {
+        gravityRun();
+    }
+
+    public void gravityRun()
+    {
+        if(ownRb == null)
+        {
+            ownRb = GetComponent<Rigidbody>();
+        }
+
         Collider[] cols = Physics.OverlapSphere(transform.position, range);
         List<Rigidbody> rbs = new List<Rigidbody>();
  
         foreach (Collider c in cols)
         {
             Rigidbody rb = c.attachedRigidbody;
-            if (rb != null && rb != ownRb && !rbs.Contains(rb))
+            if (rb != null && rb != ownRb && !rbs.Contains(rb)  && (rb.gameObject.scene.name != "SimulationTrajetory") )
             {
                 rbs.Add(rb);
                 Vector3 offset = transform.position - c.transform.position;
                 
                 Vector3 dir = offset.normalized;
 
-                rb.AddForce((gravityConstant* dir* ownRb.mass * rb.mass) / offset.sqrMagnitude);
+                Vector3 force = (gravityConstant* dir* ownRb.mass * rb.mass) / offset.sqrMagnitude;
+
+                if (!float.IsNaN(force.x) && !float.IsNaN(force.y) && !float.IsNaN(force.z) && force.magnitude < 1000f)
+                {
+                    
+                    rb.AddForce(force);
+                }
+                
             }
         }
     }
---
+++ Space Pioneers/Assets/Scripts/TrajectoryPreview.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[AddComponentMenu("SpacePioneers/Gravity/Preview")]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(LineRenderer))]
+public class TrajectoryPreview : MonoBehaviour
+{
+    [SerializeField]
+    private int maxIterations = 10;
+    private float range = 10f;
+    private Scene simulationScene;
+    private PhysicsScene simulationPhysicsScene;
+
+    private LineRenderer lineRenderer;
+
+    private List<GameObject> dummys = new List<GameObject>();
+    
+    private bool initiated;
+
+    private Rigidbody ownRb;
+
+    private GameObject twin = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initiated = false;
+
+        ownRb = this.GetComponent<Rigidbody>();
+
+    }
+
+    
+
+    void destroyAll()
+    {
+        foreach(var o in dummys){
+            Destroy(o);
+        }
+        dummys.Clear();
+    }
+    
+    bool first = true;
+
+    void simular()
+    {
+        
+        if(first)
+        {
+            twin = Instantiate(this.gameObject);
+            twin.transform.position = this.transform.position;
+            twin.transform.rotation = this.transform.rotation;
+
+            twin.GetComponent<Renderer>().enabled = false;
+            twin.GetComponent<TrajectoryPreview>().enabled = false;
+            
+
+            SceneManager.MoveGameObjectToScene(twin, simulationScene);
+
+            dummys.Add(twin);
+
+            Collider[] cols = Physics.OverlapSphere(transform.position, range);
+            List<Rigidbody> rbs = new List<Rigidbody>();
+            
+            foreach (Collider c in cols)
+            {
+                Rigidbody rb = c.attachedRigidbody;
+                if (rb != ownRb )
+                {
+                    GameObject go = c.gameObject;
+                    
+                    GameObject fakeT = Instantiate(go);
+                    fakeT.transform.position = go.transform.position;
+                    fakeT.transform.rotation = go.transform.rotation;
+                    
+
+                    Renderer fakeR = fakeT.GetComponent<Renderer>();
+                    if(fakeR){
+                        //fakeR.enabled = false;
+                    }
+                    SceneManager.MoveGameObjectToScene(fakeT, simulationScene);
+
+                    dummys.Add(fakeT);
+                }
+            }
+        }
+        first = false;
+
+        lineRenderer.positionCount = 0;
+        lineRenderer.positionCount = maxIterations;
+
+        for(int i = 0; i<maxIterations; i++)
+        {
+            /*foreach(GameObject o in dummys)
+            {
+                Gravity g = o.GetComponent<Gravity>();
+
+                if(g != null)
+                {
+                    g.gravityRun();
+                }
+            }*/
+
+            twin.GetComponent<Gravity>().gravityRun();
+
+            simulationPhysicsScene.Simulate(Time.fixedDeltaTime);
+
+            Vector3 linePosition = new Vector3(twin.transform.position.x, 
+                                                twin.transform.position.y, 
+                                                twin.transform.position.z);
+
+            lineRenderer.SetPosition(i, linePosition);
+
+            
+        }
+        //destroyAll();
+        first = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!initiated)
+        {
+            CreateSceneParameters param = new CreateSceneParameters(LocalPhysicsMode.Physics3D);
+            simulationScene = SceneManager.CreateScene("SimulationTrajetory", param);
+            simulationPhysicsScene = simulationScene.GetPhysicsScene();
+
+            lineRenderer = GetComponent<LineRenderer>();
+            lineRenderer.SetWidth(0.04f, 0.04f);
+            lineRenderer.SetColors(Color.white, Color.white);
+
+            initiated = true;
+        }
+
+        simular();
+    }
+
+
+}
