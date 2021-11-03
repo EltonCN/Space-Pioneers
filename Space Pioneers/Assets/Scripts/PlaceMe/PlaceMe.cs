153837f7230a045c42bd3ac7de7c8bb25cec9136
EltonCN <43186596+EltonCN@users.noreply.github.com>
2021-11-03 17:38:39 -0300

    Criado mecanismo para reverter ações

--- Space Pioneers/Assets/Scripts/PlaceMe/PlaceMe.cs
+++ Space Pioneers/Assets/Scripts/PlaceMe/PlaceMe.cs
@@ -28,7 +28,7 @@ public class PlaceMe : MonoBehaviour, Draggable
         mousePos.z = Camera.main.transform.position.y - desiredY;
         Vector3 worldPoint = Camera.main.ScreenToWorldPoint(mousePos);
 
-        rb.MovePosition(worldPoint);   
+        MoveTo(worldPoint);
 
         Debug.Log(worldPoint);
     }
@@ -36,5 +36,52 @@ public class PlaceMe : MonoBehaviour, Draggable
     public void OnMouseUp(InputAction.CallbackContext context)
     {
         rb.isKinematic = false;
+    }
+
+    private void MoveTo(Vector3 position)
+    {
+        rb.MovePosition(position);
+    }
+
+    public class PlaceMeSnapshot : ActionSnapshot
+    {
+        PlaceMe originator;
+        Vector3 originalPosition;
+        int cost;
+
+        public PlaceMeSnapshot(PlaceMe originator, Vector3 originalPosition, int cost = 0)
+        {
+            this.originator = originator;
+            this.originalPosition = originalPosition;
+            this.cost = cost; 
+        }
+
+        public int Cost
+        {
+            get
+            {
+                return this.cost;
+            }
+            
+            set
+            {
+                this.cost = Cost;
+            }
+        }
+
+        public void undo()
+        {
+            originator.MoveTo(originalPosition);
+        }
+
+        public int getActionCost()
+        {
+            return this.cost;
+        }
+
+        public string getActionMessage()
+        {
+            return "Mover entidade";
+        }
     }
 }
