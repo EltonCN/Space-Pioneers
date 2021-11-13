9713f339f20b7607990cd530d27996a9c26a6f7c
EltonCN <43186596+EltonCN@users.noreply.github.com>
2021-11-12 21:53:02 -0300

    Destruindo qualquer objeto que entra

--- Space Pioneers/Assets/Scripts/DestroyOnEnter.cs
+++ Space Pioneers/Assets/Scripts/DestroyOnEnter.cs
@@ -6,13 +6,38 @@ public class DestroyOnEnter : MonoBehaviour
 {
     [Tooltip("Object to wait enter in the collider.")] [SerializeField] GameObject targetObject;
 
+
+    private GameObject parent;
+
+    void Awake()
+    {
+        parent = this.transform.parent.gameObject;
+    }
+
+
     private void OnCollisionEnter(Collision other) 
     {
+        if(this.parent != null)
+        {
+            if(GameObject.ReferenceEquals(other.gameObject, this.parent))
+            {
+                return;
+            }
+
+            if(other.transform.IsChildOf(parent.transform))
+            {
+                return;
+            }
+        }
+        else if(other.transform.IsChildOf(transform))
+        {
+            return;
+        }
+        
+
         if(targetObject == null || other.gameObject == targetObject)
         {
             Destroy(other.gameObject);
         }
-
-        Debug.Log("Destruído");
     }
 }
