b261b0fccafc833a34dd58b4f06338a8586bdd23
Madu <kawakamimadu@gmail.com>
2021-11-17 18:07:13 -0300

    Camera Pan and Zoom

---
+++ Space Pioneers/Assets/Scripts/Zoom.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.InputSystem;
+
+public class Zoom : MonoBehaviour
+{
+    [SerializeField]
+    private float zoomSpeed = 20f;
+    private Camera _camera;
+    private PlayerInput playerInput;
+    InputAction zoom;
+    float scrollY;
+    
+    private void Awake() 
+    {
+        playerInput = GetComponentInParent<PlayerInput>();
+        _camera = Camera.main;
+        InputActionMap map = playerInput.currentActionMap;
+
+        zoom = map.FindAction("Zoom", true);
+        zoom.performed += ctx => OnZoom(ctx);
+        //zoom.performed += x => scrollY = x.ReadValue<float>();
+    }
+
+    /*void FixedUpdate()
+    {
+        if (scrollY < 0) {
+            ZoomScreen(-5);
+        } else if (scrollY > 0) {
+            ZoomScreen(5);
+        }
+    }*/
+    
+    public void OnZoom(InputAction.CallbackContext ctx)
+    {
+        scrollY = ctx.ReadValue<float>();
+        if (scrollY < 0) {
+            ZoomScreen(-10);    
+        } else if (scrollY > 0) {
+            ZoomScreen(10);
+        }
+    }
+
+    private void ZoomScreen(float increment) 
+    {
+        Vector3 direction = new Vector3(0, increment, 0);
+        _camera.transform.position = Vector3.MoveTowards(_camera.transform.position, 
+                                                        _camera.transform.position + direction, 
+                                                        zoomSpeed * Time.deltaTime);
+    }
+
+    public void ActivateInput()
+    {
+        playerInput.ActivateInput();
+    }
+
+    public void DeactivateInput()
+    {
+        playerInput.DeactivateInput();
+    }
+
+    public void OnEnable() 
+    {
+        ActivateInput();
+    }
+
+    public void OnDisable() 
+    {
+        DeactivateInput();
+    }
+    
+}
