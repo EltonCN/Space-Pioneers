7f7b187b3f5b294536ae63a8fa59f2d0087099ea
Madu <kawakamimadu@gmail.com>
2021-12-02 09:54:54 -0300

    Fixing play/pause button

--- Space Pioneers/Assets/Scripts/UIManager.cs
+++ Space Pioneers/Assets/Scripts/UIManager.cs
@@ -6,11 +6,12 @@ public class UIManager : MonoBehaviour
     public Button playPauseButton;
     public Sprite playButtonSprite;
     public Sprite pauseButtonSprite;
+    public Image actionBar;
     public void PlayPauseButtonChange()
     {
         if (playPauseButton.image.sprite == playButtonSprite) {
             playPauseButton.image.sprite = pauseButtonSprite;
-        } else {
+        } else if (playPauseButton.image.sprite == pauseButtonSprite && actionBar.fillAmount == 0){
             playPauseButton.image.sprite = playButtonSprite;
         }
     }
